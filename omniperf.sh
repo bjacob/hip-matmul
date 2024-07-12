4aed7829cfd1b2f35b6d8531bb21ae2028a78169
Benoit Jacob <jacob.benoit.1@gmail.com>
2024-07-12 15:59:26 -0500

    WIP

--- omniperf.sh
+++ omniperf.sh
@@ -15,4 +15,4 @@ some_profile_file="$(find "$profile_path" -name pmc_perf.csv)"
 profile_files_path="$(basename "$some_profile_file")"
 analysis_path="${profile_path}/analysis.txt"
 
-omniperf analyze -q -p "${profiles_files_path}" > "$analysis_path"
+omniperf analyze -q -p "${profile_files_path}" > "$analysis_path"
